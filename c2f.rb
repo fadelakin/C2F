1f1ac4ce66ac0ec4cd345c3f07f031b987db32bc
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-11-24 20:27:49 -0500

    fixed some stuff

--- c2f.rb
+++ c2f.rb
@@ -1,4 +1,4 @@
 print "Hello. Please enter a Celsius value: "
 celsius = gets.chomp
 fahrenheit = (celsius.to_i * 9 / 5) + 32
-print "The Fahrenheit equivalent is " + fahrenheit.to_s + "."
+puts "The Fahrenheit equivalent is " + fahrenheit.to_s + "."
