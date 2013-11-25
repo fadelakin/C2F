eda3507f00a4cc18d7bfaeeae57330386fb5dd0c
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-11-24 20:26:38 -0500

    added interactivity

--- c2f.rb
+++ c2f.rb
@@ -1,3 +1,4 @@
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
-puts "The result is: " + fahrenheit.to_s + "."
+print "Hello. Please enter a Celsius value: "
+celsius = gets.chomp
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+print "The Fahrenheit equivalent is " + fahrenheit.to_s + "."
